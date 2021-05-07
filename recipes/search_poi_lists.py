e12156a742194f28393d305911b5a9a22a1471d5
Runzhou Li (woozyking) <runzhou.li@gmail.com>
2021-05-07 14:53:12 -0400

    recipe - search POI lists function relays all pandas.Series.str.contains supported args

--- recipes/search_poi_lists.py
+++ recipes/search_poi_lists.py
@@ -1,7 +1,7 @@
 from locussdk import get_avail_poi_lists
 
 
-def search(name):
+def search(name, **kwargs):
     lists = get_avail_poi_lists('global')
 
-    return lists[lists.name.str.contains(name)]
+    return lists[lists.name.str.contains(name, **kwargs)]
